31a055aef1626364eb008327fe44a6d27bd1fbc5
MadhaviG97 <47152272+MadhaviG97@users.noreply.github.com>
2020-04-21 19:05:26 +0530

    project back end done

---
+++ back-end/src/models/models/TagProject.js
@@ -0,0 +1,26 @@
+const model = require("../model");
+const mysql = require("mysql");
+
+const attrs = [
+  "project_id",
+  "tag_id",
+  "created_at",
+  "updated_at",
+  "deleted_at",
+];
+
+function TagProject(data = {}) {
+  model.call(this, "tag_project", TagProject, data, attrs);
+}
+
+TagProject.prototype = Object.create(model.prototype);
+
+TagProject.prototype.find_by_id = function (project_id) {
+  var params = [];
+  params.push(
+    mysql.escapeId("project_id").concat(" = ").concat(mysql.escape(project_id))
+  );
+  return this.find_all(params);
+};
+
+module.exports = TagProject;
